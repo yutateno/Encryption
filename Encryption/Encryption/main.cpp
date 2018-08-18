4880d8dee2adf627644de9f889592ccd9138b3c4
NakagawaYuki <kingwasheart@yahoo.co.jp>
2018-08-18 15:36:34 +0900

    ほぼ完成

--- Encryption/Encryption/main.cpp
+++ Encryption/Encryption/main.cpp
@@ -1,7 +1,80 @@
-#include <stdio.h>
+//インクルード
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
-int main(int argc, char* argv)
-{
-	printf("adasd");
+using namespace std;
+
+//typedef宣言
+typedef unsigned char BYTE;
+typedef unsigned int UINT;
+
+//ファイルサイズを返す
+UINT file_size(ifstream &fin);
+
+//メイン関数
+int main(int argc, char *argv[]) {
+
+	if (argc < 2) {
+		cout << "ファイルをドラッグドロップしてください" << endl;
+		getchar();
+		return -1;
+	}
+
+	string path = argv[1];//入力ファイルパス
+	vector<BYTE> data;  //ファイルデータ
+	UINT size;          //ファイルサイズ
+	int rad;
+
+	{//ファイルの読み込み
+		setlocale(LC_ALL, "japanese");//ロケール設定
+
+		ifstream fin(path.c_str(), ios::binary);//ファイルオープン
+
+		if (fin.fail()) {
+			return -1; //ファイル読み込みエラー
+		}
+
+		size = file_size(fin);//ファイルサイズ取得
+		data.resize(size);//メモリ確保
+		fin.read((char*)&data[0], size);//読み込み
+
+		fin.close();
+	}
+
+	{//暗号化
+		srand(size);//乱数初期化
+		rad = rand();
+		for (UINT i = 0; i < size; i++) {//全データループ
+			data[i] = (data[i] ^ rad);//排他的論理和を取る
+		}
+	}
+
+	//解読
+	{
+		for (UINT i = 0; i < size; i++) {//全データループ
+			data[i] = (data[i] ^ rad);//排他的論理和を取る
+		}
+	}
+
+	{//保存
+		string outstr = path;
+		ofstream fout(outstr.c_str(), ios::binary);
+		fout.write((char*)&data[0], size);
+	}
+
 	return 0;
+
+}
+
+//ファイルサイズを返す
+UINT file_size(ifstream &fin) {
+	UINT pos = fin.tellg();//現在位置保存
+
+	UINT size = fin.seekg(0, ios::end).tellg();//最後にシークして位置取得→サイズ
+
+	fin.seekg(pos);//元の位置に戻す
+
+	return size;//サイズを返す
 }
