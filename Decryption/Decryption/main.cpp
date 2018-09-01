2acfccb4650ec57835c86af76aebbb9250f19aa3
NakagawaYuki <kingwasheart@yahoo.co.jp>
2018-09-01 14:36:51 +0900

    エンコードプログラムの微妙修正 デコードプログラムの一時的な保守コミット

--- Decryption/Decryption/main.cpp
+++ Decryption/Decryption/main.cpp
@@ -13,67 +13,67 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	setlocale(LC_ALL, "japanese");		// ロケール設定
 	
-	//LoadThread* loadThread = new LoadThread();					// 非同期で行うロード処理
-	//LoadMainThread* loadMainThread = new LoadMainThread();		// 非同期でメインで行うロード画面のようなものの処理
+	LoadThread* loadThread = new LoadThread();					// 非同期で行うロード処理
+	LoadMainThread* loadMainThread = new LoadMainThread();		// 非同期でメインで行うロード画面のようなものの処理
 
-	//Draw* draw = NULL;			// ロード後にロードしたファイルを渡す先
+	Draw* draw = NULL;			// ロード後にロードしたファイルを渡す先
 
-	//const int max = 10;		// ロードする個数
+	const int max = 10;		// ロードする個数
 
-	//std::string str[max];		// ロードするファイルの名前
-	//str[0] = "1.jyn";
-	//str[1] = "2.jyn";
-	//str[2] = "3.jyn";
-	//str[3] = "4.jyn";
-	//str[4] = "5.jyn";
-	//str[5] = "3.jyn";
-	//str[6] = "1.jyn";
-	//str[7] = "2.jyn";
-	//str[8] = "3.jyn";
-	//str[9] = "6.wyn";
+	std::string str[max];		// ロードするファイルの名前
+	str[0] = "1.jyn";
+	str[1] = "2.jyn";
+	str[2] = "3.jyn";
+	str[3] = "4.jyn";
+	str[4] = "5.jyn";
+	str[5] = "3.jyn";
+	str[6] = "1.jyn";
+	str[7] = "2.jyn";
+	str[8] = "3.jyn";
+	str[9] = "6.wyn";
 
-	//ELOADFILE type[max];		// ロードするファイルの形式
-	//type[0] = ELOADFILE::graph;
-	//type[1] = ELOADFILE::graph;
-	//type[2] = ELOADFILE::graph;
-	//type[3] = ELOADFILE::graph;
-	//type[4] = ELOADFILE::graph;
-	//type[5] = ELOADFILE::graph;
-	//type[6] = ELOADFILE::graph;
-	//type[7] = ELOADFILE::graph;
-	//type[8] = ELOADFILE::graph;
-	//type[9] = ELOADFILE::soundmem;
+	ELOADFILE type[max];		// ロードするファイルの形式
+	type[0] = ELOADFILE::graph;
+	type[1] = ELOADFILE::graph;
+	type[2] = ELOADFILE::graph;
+	type[3] = ELOADFILE::graph;
+	type[4] = ELOADFILE::graph;
+	type[5] = ELOADFILE::graph;
+	type[6] = ELOADFILE::graph;
+	type[7] = ELOADFILE::graph;
+	type[8] = ELOADFILE::graph;
+	type[9] = ELOADFILE::soundmem;
 
-	//int flag = 0;			// シーン移動を簡略的に行うための変数
-	//bool threadEndFlag = false;
+	int flag = 0;			// シーン移動を簡略的に行うための変数
+	bool threadEndFlag = false;
 
-	Draw* draw = new Draw();
+	//Draw* draw = new Draw();
 
 	// 本編
 	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) != 1)
 	{
-		//if (flag == 0)		// ロードを行う
-		//{
-		//	loadThread->Run(max, str, type);
-		//	loadMainThread->Run();
+		if (flag == 0)		// ロードを行う
+		{
+			loadThread->Run(max, str, type);
+			loadMainThread->Run();
 
-		//	threadEndFlag = loadThread->Stop();
-		//	loadMainThread->SetThreadEndFlag(threadEndFlag);
-		//	flag = loadMainThread->Stop();
-		//}
-		//else if (flag == 1)		// ロードからシーンを移り変わる
-		//{
-		//	draw = new Draw(loadThread->GetFile(0), loadThread->GetFile(1), loadThread->GetFile(2), loadThread->GetFile(3), loadThread->GetFile(4), loadThread->GetFile(5), loadThread->GetFile(6), loadThread->GetFile(7), loadThread->GetFile(8), loadThread->GetFile(9));
-		//	delete loadThread;
-		//	delete loadMainThread;
+			threadEndFlag = loadThread->Stop();
+			loadMainThread->SetThreadEndFlag(threadEndFlag);
+			flag = loadMainThread->Stop();
+		}
+		else if (flag == 1)		// ロードからシーンを移り変わる
+		{
+			draw = new Draw(loadThread->GetFile(0), loadThread->GetFile(1), loadThread->GetFile(2), loadThread->GetFile(3), loadThread->GetFile(4), loadThread->GetFile(5), loadThread->GetFile(6), loadThread->GetFile(7), loadThread->GetFile(8), loadThread->GetFile(9));
+			delete loadThread;
+			delete loadMainThread;
 
-		//	flag = 2;
-		//}
-		//else		// ロード後のシーン処理
-		//{
-		//	draw->Process();
-		//}
-		draw->Process();
+			flag = 2;
+		}
+		else		// ロード後のシーン処理
+		{
+			draw->Process();
+		}
+		//draw->Process();
 	}
 
 	delete draw;
--- Encryption/Encryption/main.cpp
+++ Encryption/Encryption/main.cpp
@@ -1,4 +1,3 @@
-//インクルード
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -14,9 +13,11 @@ typedef unsigned int UINT;
 UINT file_size(ifstream &fin);
 
 //メイン関数
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-	if (argc < 2) {
+	if (argc < 2)
+	{
 		cout << "ファイルをドラッグドロップしてください" << endl;
 		getchar();
 		return -1;
@@ -27,37 +28,35 @@ int main(int argc, char *argv[]) {
 	UINT size;          //ファイルサイズ
 	int rad = 0x2546;
 
-	{//ファイルの読み込み
-		setlocale(LC_ALL, "japanese");//ロケール設定
+	//ファイルの読み込み
+	setlocale(LC_ALL, "japanese");//ロケール設定
 
-		ifstream fin(path.c_str(), ios::binary);//ファイルオープン
+	ifstream fin(path.c_str(), ios::binary);//ファイルオープン
 
-		if (fin.fail()) {
-			return -1; //ファイル読み込みエラー
-		}
-
-		size = file_size(fin);//ファイルサイズ取得
-		data.resize(size);//メモリ確保
-		fin.read((char*)&data[0], size);//読み込み
-
-		fin.close();
+	if (fin.fail()) {
+		return -1; //ファイル読み込みエラー
 	}
 
-	{//暗号化
-		for (UINT i = 0; i < size; i += 5) {//全データループ
-			data[i] = (data[i] ^ rad);//排他的論理和を取る
-		}
-	}
+	size = file_size(fin);//ファイルサイズ取得
+	data.resize(size);//メモリ確保
+	fin.read((char*)&data[0], size);//読み込み
 
-	{//保存
-		string outstr = path;
-		outstr.erase(outstr.end() - 2, outstr.end());
-		outstr.append("yn");
-		ofstream fout(outstr.c_str(), ios::binary);
-		fout.write((char*)&data[0], size);
-		fout.close();
-	}
+	fin.close();
+	
 
+	//暗号化
+	for (UINT i = 0; i < size; i += 5) {//全データループ
+		data[i] = (data[i] ^ rad);//排他的論理和を取る
+	}
+	
+	//保存
+	string outstr = path;
+	outstr.erase(outstr.end() - 2, outstr.end());
+	outstr.append("yn");
+	ofstream fout(outstr.c_str(), ios::binary);
+	fout.write((char*)&data[0], size);
+	fout.close();
+	
 	printf("暗号化完了");
 	getchar();
 
@@ -66,7 +65,8 @@ int main(int argc, char *argv[]) {
 }
 
 //ファイルサイズを返す
-UINT file_size(ifstream &fin) {
+UINT file_size(ifstream &fin)
+{
 	UINT pos = fin.tellg();//現在位置保存
 
 	UINT size = fin.seekg(0, ios::end).tellg();//最後にシークして位置取得→サイズ
